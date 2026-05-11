2ce8fac6fb4b48a27553d8d6a7678c62b404e732
SomeshKumar <someshkumar8211@gmail.com>
2026-05-11 21:18:57 +0530

    clean-up

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
-import './styles.css'
+import type { Metadata } from 'next'
 
-export const metadata = {
+export const metadata: Metadata = {
   description: 'A blank template using Payload in a Next.js app.',
   title: 'Payload Blank Template',
 }
