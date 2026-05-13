fe7625cfa87d392e8a36be77d07eb767e2cbe362
SomeshKumar <someshkumar8211@gmail.com>
2026-05-13 17:55:17 +0530

    feat(media): blur data urls

--- next.config.ts
+++ next.config.ts
@@ -2,9 +2,14 @@ import { withPayload } from '@payloadcms/next/withPayload'
 import type { NextConfig } from 'next'
 import path from 'path'
 import { fileURLToPath } from 'url'
+import withPlaiceholder from '@plaiceholder/next'
 
 const __filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(__filename)
+
+/**
+ * @type {import('next').NextConfig}
+ */
 
 const nextConfig: NextConfig = {
   images: {
@@ -28,4 +33,4 @@ const nextConfig: NextConfig = {
   },
 }
 
-export default withPayload(nextConfig, { devBundleServerPackages: false })
+export default withPlaiceholder(withPayload(nextConfig, { devBundleServerPackages: false }))
